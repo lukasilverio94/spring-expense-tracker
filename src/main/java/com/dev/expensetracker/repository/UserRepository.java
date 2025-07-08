5cd6068e0eb0251176436378b376dc95ca82b55c
lukasilverio <lukasilverio94@gmail.com>
2025-07-08 22:33:04 +0200

    chore: remove unused repository

--- src/main/java/com/dev/expensetracker/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dev.expensetracker.repository;
-
-import com.dev.expensetracker.domain.entity.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<AppUser, UUID> {
-}
